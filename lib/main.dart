6fd67150ad198f35d753d27b13a2d75e6e7ae00d
fumiyasac <just1factory@gmail.com>
2025-02-23 08:15:11 +0900

    Firebaseを利用できる様にする＆Firestoreのパッケージを追加する

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
 import 'package:todo_style_example_app/widget/card_todo_widget.dart';
 import 'common/add_new_task_model.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const ProviderScope(
       child: MyApp()
