52d801a016b371dd9ed12064226e154a3b957cbf
fumiyasac <just1factory@gmail.com>
2025-02-22 22:52:28 +0900

    Card用のViewサンプルを作成

--- lib/main.dart
+++ lib/main.dart
@@ -141,17 +141,47 @@ class HomePage extends StatelessWidget {
                 child: Row(
                   children: [
                     Container(
-                      color: Colors.red,
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(12),
+                          bottomLeft: Radius.circular(12),
+                        ),
+                      ),
                       width: 20,
                     ),
                     Expanded(
                       child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        padding: EdgeInsets.symmetric(horizontal: 12),
                         child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             ListTile(
+                              contentPadding: EdgeInsets.zero,
                               title: Text('Learning Web Developer'),
-                            )
+                              subtitle: Text('Learning topic HTML and CSS'),
+                              trailing: Transform.scale(
+                                scale: 1.5,
+                                child: Checkbox(
+                                  activeColor: Colors.blue.shade800,
+                                  shape: CircleBorder(),
+                                  value: true,
+                                  onChanged: (value) => print(value),
+                                ),
+                              ),
+                            ),
+                            Divider(
+                              thickness: 1.5,
+                              color: Colors.grey.shade200,
+                            ),
+                            Row(
+                              children: [
+                                Text('Today'),
+                                Gap(12),
+                                Text('09:15 PM - 11:45 PM'),
+                              ],
+                            ),
                           ],
                         ),
                       ),
